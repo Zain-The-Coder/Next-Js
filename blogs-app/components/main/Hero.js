b3a3b052aafd365bb79683d7ca191d2b24d8f157
Zain-The-Coder <zain015976@gmail.com>
2026-02-11 08:03:31 +0500

    Next Js

--- blogs-app/components/main/Hero.js
+++ blogs-app/components/main/Hero.js
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <>
     <div className='text-center font-bold text-[33px] py-[40px] text-black'>Popular Blogs</div>
-    <section className='flex justify-around'>
-    <Blogs src="/images/image-1.webp" alt="first-image" dur="2 days ago" para="First Blog" />
-    <Blogs src="/images/image-2.jpg" alt="second-image" dur="Yesterday" para="Second Blog" />
-    <Blogs src="/images/image-3.webp" alt="third-image" dur="1 week ago" para="Third Blog" />
-    </section>
+<section className='flex flex-col items-center sm:flex-row sm:justify-around'>
+  <Blogs src="/images/image-1.webp" alt="first-image" dur="2 days ago" para="First Blog" />
+  <Blogs src="/images/image-2.jpg" alt="second-image" dur="Yesterday" para="Second Blog" />
+  <Blogs src="/images/image-3.webp" alt="third-image" dur="1 week ago" para="Third Blog" />
+</section>
     
     </>
   )
